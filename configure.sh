3184b5068fba2c6228415cc6d01399f2f9af646d
iamadahn <vendonism16@yandex.ru>
2025-06-01 18:52:33 +0300

    Userleds are usable now

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@ cd nuttx-apps
 ln -s ../app custom_app
 cd ../nuttx
 make distclean
-./tools/configure.sh -l ../board/configs/nsh
+./tools/configure.sh -l ../board/configs/iamwebmorda
 cd ../nuttx-apps
 rm -rf custom_app
 
