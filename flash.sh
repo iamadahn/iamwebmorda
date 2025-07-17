8a1585e656308e81561dbe12f72588f433212f77
iamadahn <vendonism16@yandex.ru>
2025-07-17 23:15:10 +0300

    build and flash fix

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-openocd -f interface/stlink.cfg -f openocd.cfg -c init -c halt -c "flash write_image erase build/nuttx.bin 0x90000000" -c reset -c shutdown
+openocd -f interface/stlink.cfg -f openocd.cfg -c init -c halt -c "flash write_image erase output/nuttx.bin 0x90000000" -c reset -c shutdown
